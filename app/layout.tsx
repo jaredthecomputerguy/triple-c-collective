aa3457968375e530d8541e7954858662812ea660
Jared Mercer <jaredthebackpacker@gmail.com>
2023-12-26 11:02:33 -0800

    chore: fix typo in default layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Home |Triple C Collective",
+  title: "Home | Triple C Collective",
   description:
     "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.",
 };
