7d1547846216bf92d5a636fce74f3247b2065935
Jared <jaredthebackpacker@gmail.com>
2023-12-27 11:45:25 -0800

    chore: fix background image on age-consent page

--- app/age-consent/page.tsx
+++ app/age-consent/page.tsx
@@ -2,7 +2,7 @@ import { AgeConsentForm } from "./age-consent-form";
 
 export default function AgeConsentPage() {
   return (
-    <main className="bg-[url('/lake-county.webp')] min-h-screen w-screen flex flex-col items-center justify-center">
+    <main className="bg-[url('/lake-county.webp')] bg-cover min-h-screen w-screen flex flex-col items-center justify-center">
       <AgeConsentForm />
     </main>
   );
