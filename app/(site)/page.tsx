224dfeac13e11a02143232bc274d25f2318badeb
Jared Mercer <jaredthebackpacker@gmail.com>
2023-12-30 11:41:33 -0800

    chore: fix metadata

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,15 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Home | Triple C Collective',
+    description:
+        'Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.',
+};
+
 export default function HomePage() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    );
 }
--- app/age-consent/page.tsx
+++ app/age-consent/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next';
 import { AgeConsentForm } from './age-consent-form';
+
+export const metadata: Metadata = {
+    title: 'Age Consent | Triple C Collective',
+    description:
+        'Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.',
+};
 
 export default function AgeConsentPage() {
     return (
