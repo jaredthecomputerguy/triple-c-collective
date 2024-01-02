13bc65d80eda30131cf27cc3985c866f164a41c9
Jared <jaredthebackpacker@gmail.com>
2024-01-01 16:02:11 -0800

    chore: add metadata to all known routes

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,19 +1,26 @@
 // @ts-expect-error Experimental
-import { GoogleMapsEmbed } from '@next/third-parties/google';
+import { GoogleMapsEmbed } from "@next/third-parties/google";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Contact Us | Triple C Collective",
+  description:
+    "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.",
+};
 
 export default function ContactPage() {
-    // TODO: Implement this
-    return (
-        <section>
-            Contact Page
-            <GoogleMapsEmbed
-                apiKey={process.env.GOOGLE_MAPS_API_KEY!}
-                mode='place'
-                allowfullscreen
-                style='height: 500px; width: 500px'
-                loading='eager'
-                q='Triple C Collective, 14196 Lakeshore Ave. Clearlake CA 95422'
-            />
-        </section>
-    );
+  // TODO: Implement this
+  return (
+    <section>
+      Contact Page
+      <GoogleMapsEmbed
+        apiKey={process.env.GOOGLE_MAPS_API_KEY!}
+        mode="place"
+        allowfullscreen
+        style="height: 500px; width: 500px"
+        loading="eager"
+        q="Triple C Collective, 14196 Lakeshore Ave. Clearlake CA 95422"
+      />
+    </section>
+  );
 }
--- app/(site)/products/page.tsx
+++ app/(site)/products/page.tsx
@@ -1,4 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Products | Triple C Collective",
+  description:
+    "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.",
+};
+
 export default function ProductsPage() {
-    // TODO: Implement this
-    return <section>Products Page</section>;
+  // TODO: Implement this
+  return <section>Products Page</section>;
 }
