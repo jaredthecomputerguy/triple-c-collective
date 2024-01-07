2939d7d67a0191f640c47644cc2a07170ddb49c7
Jared Mercer <jaredthebackpacker@gmail.com>
2024-01-07 13:09:11 -0800

    chore: remove console log

--- app/(site)/blogs/page.tsx
+++ app/(site)/blogs/page.tsx
@@ -1,23 +1,21 @@
-import { Metadata } from "next";
-import getPayloadClient from "../../../payload/payloadClient";
+import { Metadata } from 'next';
+import getPayloadClient from '../../../payload/payloadClient';
 
 export const metadata: Metadata = {
-  title: "Blogs | Triple C Collective",
-  description:
-    "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.",
+    title: 'Blogs | Triple C Collective',
+    description:
+        'Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.',
 };
 
 export default async function BlogsPage() {
-  // TODO: Implement this
-  const payload = await getPayloadClient();
+    // TODO: Implement this
+    const payload = await getPayloadClient();
 
-  const blogs = await payload.find({
-    collection: "blogs",
-    limit: 10,
-    sort: "createdAt",
-  });
+    const blogs = await payload.find({
+        collection: 'blogs',
+        limit: 10,
+        sort: 'createdAt',
+    });
 
-  console.log("blogs", blogs.docs);
-
-  return <main>Blogs Page</main>;
+    return <main>Blogs Page</main>;
 }
