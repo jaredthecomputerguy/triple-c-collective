ad987761d7b391073d37f74b21d48ec691ff1de4
Jared Mercer <jaredthebackpacker@gmail.com>
2024-01-15 12:02:06 -0800

    chore: add husky and lint-staged

--- app/(site)/blogs/seed-blogs.tsx
+++
@@ -1,8 +0,0 @@
-import { blogPostsToSeed } from '@/app/lib/seed-data';
-import { seedBlogs } from '@/app/lib/utils';
-
-export const SeedBlogs = () => {
-    seedBlogs(blogPostsToSeed);
-
-    return <button></button>;
-};
