028cb93fce562e832da1b6d54d9640d1ab9c6415
Jared Mercer <jaredthebackpacker@gmail.com>
2024-01-15 11:44:38 -0800

    chore: add styles to blog cards, change z index of header to be the topmost element

--- app/(site)/blogs/blog-card.tsx
+++ app/(site)/blogs/blog-card.tsx
@@ -10,7 +10,9 @@ interface BlogCardProps {
 export const BlogCard = ({ blog }: BlogCardProps) => {
     return (
         <>
-            <Link href={`/blogs/${blog.id}`} className='flex flex-col gap-1 overflow-hidden rounded-lg bg-slate-100'>
+            <Link
+                href={`/blogs/${blog.id}`}
+                className='flex flex-col gap-1 p-1 overflow-hidden transition-transform ease-in-out rounded-lg outline-none bg-slate-100 focus:scale-105 active:scale-105 hover:scale-105 focus:outline-primary-purple hover:outline-primary-purple active:outline-primary-purple'>
                 <Image src='/images/generic-blog.jpg' width={600} height={400} alt='Blog image' />
                 <div className='flex flex-col gap-1 p-4'>
                     <span className='text-xl font-semibold'>{blog.title}</span>
