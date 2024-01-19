d02f3631a30b87c1dbbb8398d649e9418c46b9a6
Jared Mercer <jaredthebackpacker@gmail.com>
2024-01-19 14:05:50 -0800

    fix: move google maps api key to client .env to fix issue with client side routing

--- app/(site)/contact/google-map-embed.tsx
+++ app/(site)/contact/google-map-embed.tsx
@@ -1,18 +1,18 @@
-'use client';
+"use client";
 
 // @ts-expect-error Experimental package
-import { GoogleMapsEmbed } from '@next/third-parties/google';
+import { GoogleMapsEmbed } from "@next/third-parties/google";
 
 export const GoogleMapEmbed = () => {
-    return (
-        <GoogleMapsEmbed
-            apiKey={process.env.GOOGLE_MAPS_API_KEY!}
-            mode='place'
-            allowfullscreen
-            style='height: 100%; width: 100%'
-            loading='eager'
-            q='Triple C Collective, 14196 Lakeshore Ave. Clearlake CA 95422'
-            title='Google Maps'
-        />
-    );
+  return (
+    <GoogleMapsEmbed
+      apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY!}
+      mode="place"
+      allowfullscreen
+      style="height: 100%; width: 100%"
+      loading="eager"
+      q="Triple C Collective, 14196 Lakeshore Ave. Clearlake CA 95422"
+      title="Google Maps"
+    />
+  );
 };
