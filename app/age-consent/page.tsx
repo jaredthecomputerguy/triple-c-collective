40dd94455a3816ac06a8da298b3cf18417ced232
Jared Mercer <jaredthebackpacker@gmail.com>
2024-01-20 15:35:46 -0800

    fix: change url for age consent background

--- app/age-consent/page.tsx
+++ app/age-consent/page.tsx
@@ -1,16 +1,16 @@
-import { Metadata } from 'next';
-import { AgeConsentForm } from './age-consent-form';
+import { Metadata } from "next";
+import { AgeConsentForm } from "./age-consent-form";
 
 export const metadata: Metadata = {
-    title: 'Home | Triple C Collective',
-    description:
-        'Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.',
+  title: "Home | Triple C Collective",
+  description:
+    "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.",
 };
 
 export default function AgeConsentPage() {
-    return (
-        <main className="bg-[url('/lake-county.webp')] bg-cover min-h-screen w-screen flex flex-col items-center justify-center">
-            <AgeConsentForm />
-        </main>
-    );
+  return (
+    <main className="bg-[url('/images/lake-county.webp')] bg-cover min-h-screen w-screen flex flex-col items-center justify-center">
+      <AgeConsentForm />
+    </main>
+  );
 }
