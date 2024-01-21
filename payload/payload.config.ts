f54b7da28db87117d2b95eb87ea7fa398dc0d2da
Jared Mercer <jaredthebackpacker@gmail.com>
2024-01-21 12:36:06 -0800

    fix: change s3 secret key to non-reserved name

--- payload/payload.config.ts
+++ payload/payload.config.ts
@@ -36,7 +36,7 @@ export default buildConfig({
 
               credentials: {
                 accessKeyId: process.env.AWS_KEY!,
-                secretAccessKey: process.env.AWS_SECRET_KEY!,
+                secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
               },
             },
 
