7faa64ffd15f9ec902ecb033b3108438c31977cc
Jared Mercer <jaredthebackpacker@gmail.com>
2024-02-10 11:14:20 -0800

    feat: add social links to contact page

--- app/(site)/contact/page.tsx
+++ app/(site)/contact/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import { GoogleMapEmbed } from "./google-map-embed";
 import { ContactForm } from "./contact-form";
+import { FacebookIcon } from "@/app/_components/icons/facebook-icon";
+import { InstagramIcon } from "@/app/_components/icons/instagram-icon";
 
 export const dynamic = "force-dynamic";
 
@@ -51,6 +53,23 @@ export default function ContactPage() {
               <a className="text-sky-700 font-medium" href="mailto:clearlakecompassioncenter@yahoo.com">
                 clearlakecompassioncenter@yahoo.com
               </a>
+            </div>
+            <div className="flex flex-col gap-2">
+              <p className="text-xl font-semibold">Socials</p>
+              <ul className="flex gap-2 items-center">
+                <li className="p-1">
+                  <a
+                    href="https://www.facebook.com/people/Triple-C-Collective-Clearlake-CA/100095379903282/"
+                    target="_blank">
+                    <FacebookIcon className="w-8 h-8" />
+                  </a>
+                </li>
+                <li className="p-1">
+                  <a href="https://www.instagram.com/_tripleccollective/" target="_blank">
+                    <InstagramIcon className="w-9 h-9" />
+                  </a>
+                </li>
+              </ul>
             </div>
             <hr />
             <div>
--- app/_components/icons/facebook-icon.tsx
+++ app/_components/icons/facebook-icon.tsx
@@ -1,18 +1,8 @@
 import { SVGProps } from "react";
 
-export const FacebookIcon = ({
-  className = "w-6 h-6",
-  stroke = "currentColor",
-  strokeWidth = 1.5,
-}: SVGProps<SVGElement>) => {
+export const FacebookIcon = ({ className = "w-6 h-6" }: SVGProps<SVGElement>) => {
   return (
-    <svg
-      className={className}
-      stroke={stroke}
-      strokeWidth={strokeWidth}
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 320 512"
-    >
+    <svg className={className} fill="#4267B2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
       <path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z" />
     </svg>
   );
