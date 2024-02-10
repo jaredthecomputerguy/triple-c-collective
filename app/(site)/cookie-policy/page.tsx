fb3cc233890f3a36ab7adb175902452c45095a6b
Jared Mercer <jaredthebackpacker@gmail.com>
2024-02-10 09:57:06 -0800

    chore: change the top margin to something smaller for the cookie policy page

--- app/(site)/cookie-policy/page.tsx
+++ app/(site)/cookie-policy/page.tsx
@@ -11,66 +11,54 @@ export default function CookiePolicyPage() {
   return (
     <div className="max-w-4xl mx-auto sm:py-12 py-6 bg-[#fefefe] px-4 prose-a:text-blue-600 prose prose-h5:font-semibold prose-h6:text-[#111827] prose-h5:text-[#111827] prose-h6:font-semibold">
       <h1 className="text-4xl font-semibold">Cookie Policy</h1>
-      <span className="mx-auto text-center text-gray-700">
-        Last Updated: 12/31/2023
-      </span>
+      <span className="mx-auto text-center text-gray-700">Last Updated: 12/31/2023</span>
       <hr className="my-1" />
       <article className="pb-6">
-        <h2 className="text-3xl">What Are Cookies</h2>
+        <h2 className="text-3xl mt-6">What Are Cookies</h2>
         <p>
-          As is common practice with almost all professional websites this site
-          uses cookies, which are tiny files that are downloaded to your
-          computer, to improve your experience.
+          As is common practice with almost all professional websites this site uses cookies, which are tiny files that
+          are downloaded to your computer, to improve your experience.
         </p>
         <p>
-          This page describes what information they gather, how we use it and
-          why we sometimes need to store these cookies. We will also share how
-          you can prevent these cookies from being stored however this may
-          downgrade or &apos;break&apos; certain elements of the sites
-          functionality.
+          This page describes what information they gather, how we use it and why we sometimes need to store these
+          cookies. We will also share how you can prevent these cookies from being stored however this may downgrade or
+          &apos;break&apos; certain elements of the sites functionality.
         </p>
         <h3 className="py-2 text-3xl">How We Use Cookies</h3>
         <p>
-          We use cookies for a variety of reasons detailed below. Unfortunately
-          in most cases there are no industry standard options for disabling
-          cookies without completely disabling the functionality and features
-          they add to this site.
+          We use cookies for a variety of reasons detailed below. Unfortunately in most cases there are no industry
+          standard options for disabling cookies without completely disabling the functionality and features they add to
+          this site.
         </p>
         <p>
-          It is recommended that you leave on all cookies if you are not sure
-          whether you need them or not in case they are used to provide a
-          service that you use.
+          It is recommended that you leave on all cookies if you are not sure whether you need them or not in case they
+          are used to provide a service that you use.
         </p>
         <h4 className="py-2 text-3xl">Disabling Cookies</h4>
         <p>
-          You can prevent the setting of cookies by adjusting the settings on
-          your browser (see your browser Help for how to do this).
+          You can prevent the setting of cookies by adjusting the settings on your browser (see your browser Help for
+          how to do this).
         </p>
         <p>
-          Be aware that disabling cookies will affect the functionality of this
-          and many other websites that you visit. Disabling cookies will usually
-          result in also disabling certain functionality and features of the
-          this site.
+          Be aware that disabling cookies will affect the functionality of this and many other websites that you visit.
+          Disabling cookies will usually result in also disabling certain functionality and features of the this site.
         </p>
         <p>Therefore it is recommended that you do not disable cookies.</p>
         <h5 className="py-2 text-3xl">The Cookies We Set</h5>
         <p>
-          In order to provide you with a great experience on this site we
-          provide the functionality to set your preferences for how this site
-          runs when you use it. In order to remember your preferences we need to
-          set cookies so that this information can be called whenever you
-          interact with a page is affected by your preferences.
+          In order to provide you with a great experience on this site we provide the functionality to set your
+          preferences for how this site runs when you use it. In order to remember your preferences we need to set
+          cookies so that this information can be called whenever you interact with a page is affected by your
+          preferences.
         </p>
         <h6 className="py-2 text-3xl">More Information</h6>
         <p>
-          Hopefully that has clarified things for you and as was previously
-          mentioned if there is something that you aren&apos;t sure whether you
-          need or not it&apos;s usually safer to leave cookies enabled in case
-          it does interact with one of the features you use on our site.
+          Hopefully that has clarified things for you and as was previously mentioned if there is something that you
+          aren&apos;t sure whether you need or not it&apos;s usually safer to leave cookies enabled in case it does
+          interact with one of the features you use on our site.
         </p>
         <p>
-          However if you are still looking for more information then you can
-          contact us via our{" "}
+          However if you are still looking for more information then you can contact us via our{" "}
           <Link className="text-blue-500" href="/contact">
             contact page
           </Link>
