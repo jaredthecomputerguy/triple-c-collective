162fee9338a3590423eb3edeec0714e17d52f2df
Jared Mercer <jaredthebackpacker@gmail.com>
2024-02-13 17:31:25 -0800

    chore: added psuedo styles to links/buttons that needed them

--- app/brand-carousel.tsx
+++ app/brand-carousel.tsx
@@ -4,13 +4,13 @@ import brands from "@/lib/data/brands.json";
 
 export const BrandCarousel = () => {
   return (
-    <div className="flex gap-8 py-6 overflow-x-hidden items-center">
+    <div className="flex gap-8 py-6 overflow-x-hidden items-center outline-none rounded-lg focus:outline-primary-purple active:outline-primary-purple">
       {brands.map((brand) => (
         <Link
           key={brand.id}
           href={brand.website}
           target="_blank"
-          className="shrink-0 h-full animate-mobile-scroll sm:animate-desktop-scroll">
+          className="shrink-0 h-full animate-mobile-scroll sm:animate-desktop-scroll outline-none rounded-lg focus:outline-primary-purple active:outline-primary-purple">
           <Image
             className="aspect-square object-contain w-32 h-32 sm:w-48 sm:h-48"
             src={brand.src!}
