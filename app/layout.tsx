8a3d48e465466a7787bf68d1ba4147fabb229e11
Jared Mercer <jaredthebackpacker@gmail.com>
2024-03-26 20:53:35 -0700

    chore: add metadata base

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ export const metadata: Metadata = {
   title: "Home | Triple C Collective",
   description:
     "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.",
+  metadataBase: new URL("https://triple-c-collective.vercel.app.com"),
 };
 
 const montserrat = Montserrat({
