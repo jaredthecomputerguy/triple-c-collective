4061a10cb1a16ef59676a2368f96082c67a83dbf
Jared Mercer <jaredthebackpacker@gmail.com>
2024-03-27 11:16:21 -0700

    chore: add a ton of new metadata

--- app/(main-site)/page.tsx
+++ app/(main-site)/page.tsx
@@ -3,13 +3,9 @@ import Image from "next/image";
 import Link from "next/link";
 import dynamic from "next/dynamic";
 
-import { ReviewCard } from "../_components/review-card";
-import { StoreIcon } from "../_components/icons/store";
-import { StarRating } from "../_components/star-rating";
-
-const BrandCarousel = dynamic(() =>
-  import("../_components/brand-carousel").then((mod) => mod.BrandCarousel),
-);
+import { ReviewCard } from "@/app/_components/review-card";
+import { StoreIcon } from "@/app/_components/icons/store";
+import { StarRating } from "@/app/_components/star-rating";
 
 import reviews from "@/lib/data/reviews.json";
 
@@ -18,22 +14,64 @@ import logoImg from "@/public/images/logo.png";
 import orderOnlineImg from "@/public/images/order-online.avif";
 import phonecallImg from "@/public/images/phonecall.avif";
 import rewardsImg from "@/public/images/rewards.avif";
+
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
-} from "../_components/accordian";
+} from "@/app/_components/accordian";
 
 export const metadata: Metadata = {
   title: "Home | Triple C Collective",
   description:
     "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.",
+  keywords: [
+    "cannabis",
+    "dispensary",
+    "marijuana",
+    "weed",
+    "pot",
+    "Lake County",
+    "California",
+    "Triple C Collective",
+    "flower",
+    "dab",
+    "concentrate",
+    "edibles",
+    "cbd",
+    "kratom",
+    "wellness",
+    "Clearlake",
+  ],
+  authors: [
+    {
+      name: "Jared Mercer",
+      url: "https://jaredthecomputerguy.dev",
+    },
+  ],
+  creator: "Jared Mercer",
   openGraph: {
     title: "Home | Triple C Collective",
+    url: "https://triple-c-collective.vercel.app",
+    description: "Lake County's Premier Cannabis Dispensary",
     images: "https://triple-c-collective.vercel.app/opengraph-image.png",
+    siteName: "Triple C Collective",
+    locale: "en_US",
+    type: "website",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "Home | Triple C Collective",
+    description: "Lake County's Premier Cannabis Dispensary",
+    images: ["https://triple-c-collective.vercel.app/opengraph-image.png"],
+  },
+  metadataBase: new URL("https://triple-c-collective.vercel.app"),
 };
+
+const BrandCarousel = dynamic(() =>
+  import("../_components/brand-carousel").then((mod) => mod.BrandCarousel),
+);
 
 export default function HomePage() {
   return (
