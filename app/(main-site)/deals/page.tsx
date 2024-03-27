801824adc2c7968aef2bb93db70e1eedc383eaef
Jared Mercer <jaredthebackpacker@gmail.com>
2024-03-27 13:08:28 -0700

    chore: add empty deals state to deals page

--- app/(main-site)/deals/page.tsx
+++ app/(main-site)/deals/page.tsx
@@ -2,6 +2,9 @@ import { Metadata } from "next";
 import { TrapTakeover } from "./trap-takeover";
 import { DealCards } from "./deal-cards";
 import getPayloadClient from "@/payload/payloadClient";
+import Image from "next/image";
+import { Button } from "@/app/_components/button";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Deals | Triple C Collective",
@@ -75,7 +78,33 @@ export default async function DealsPage() {
             Current Deals
           </h1>
           <hr className="pb-4" />
-          <DealCards deals={deals.docs} />
+          {deals.docs.length === 0 ? (
+            <div className="flex flex-col items-center justify-center gap-12 py-8 md:py-16">
+              <Image
+                className="h-96 w-96"
+                src="/images/empty-deals.svg"
+                alt="No deals"
+                width={300}
+                height={300}
+              />
+              <p className="text-center text-3xl">
+                There are no active promotions.
+              </p>
+              <Button
+                className="rounded bg-primary-purple px-6 py-6 font-semibold text-white outline-none transition-all hover:bg-primary-purple/80 focus:bg-primary-purple/80 focus:outline-primary-purple disabled:bg-primary-purple/50 md:text-xl"
+                asChild
+              >
+                <Link
+                  href="https://triplec.treez.io/onlinemenu/?customerType=ADULT"
+                  target="_blank"
+                >
+                  Shop All Products
+                </Link>
+              </Button>
+            </div>
+          ) : (
+            <DealCards deals={deals.docs} />
+          )}
         </section>
       </div>
     </main>
