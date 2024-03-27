de56591510a9f4a10d551b83bf73a0f62f974699
Jared Mercer <jaredthebackpacker@gmail.com>
2024-03-27 11:18:47 -0700

    chore: shorten the meta description

--- app/(main-site)/page.tsx
+++ app/(main-site)/page.tsx
@@ -25,7 +25,7 @@ import {
 export const metadata: Metadata = {
   title: "Home | Triple C Collective",
   description:
-    "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.",
+    "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California.",
   keywords: [
     "cannabis",
     "dispensary",
