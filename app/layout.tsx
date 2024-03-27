52e4e5d147b29c190dbacabcb8ead9739561db3a
Jared Mercer <jaredthebackpacker@gmail.com>
2024-03-27 10:21:11 -0700

    chore: add explicit opengraph url

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,8 @@ export const metadata: Metadata = {
   title: "Home | Triple C Collective",
   description:
     "Explore the best in medicinal and recreational cannabis at Triple C Collective, serving Lake County, California. Proudly open for over 15 years, we offer a diverse selection of high-quality cannabis products, expert guidance, and a welcoming environment for cannabis enthusiasts. Discover a trusted name in the industry - Triple C Collective, your premier destination for a decade and a half of cannabis excellence.",
-  metadataBase: new URL("https://triple-c-collective.vercel.app.com"),
   openGraph: {
-    images: "/opengraph-image.png",
+    images: "https://triple-c-collective.vercel.app/opengraph-image.png",
   },
 };
 
