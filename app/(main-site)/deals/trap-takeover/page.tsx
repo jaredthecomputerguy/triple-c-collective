19228bce3b55e8473ad7e9cc25e28303b774b6f6
Jared Mercer <jaredthebackpacker@gmail.com>
2024-06-07 09:58:18 -0700

    chore: removed unused WIP header on trap takeover

--- app/(main-site)/deals/trap-takeover/page.tsx
+++ app/(main-site)/deals/trap-takeover/page.tsx
@@ -4,8 +4,15 @@ export default function TrapTakeoverPage() {
   return (
     <main className="bg-[#fefefe]">
       <div className="mx-auto max-w-7xl bg-[#fefefe] px-4 py-6 sm:py-12">
-        <h1 className="py-4 font-logo text-4xl font-semibold">Trap Takeover</h1>
-        <hr className="pb-12" />
+        <span className="px-1 pb-1 text-gray-600">June 7th, 2024</span>
+        <h1 className="pb-4 font-logo text-4xl font-semibold">Trap Takeover</h1>
+        <hr className="pb-4" />
+        <section>
+          {/* <h2 className="py-4 font-logo text-3xl font-semibold">
+             Featured Brands
+           </h2>
+          <p className="py-4 text-lg"></p> */}
+        </section>
         <ImageViewer
           className="mx-auto h-full w-full cursor-pointer object-cover object-top"
           src="/images/trap-takeover/06072024_traptakeover.webp"
