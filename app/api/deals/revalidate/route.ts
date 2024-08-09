eb2a44301fe49ce67d85e3aa8b3104d63e474f34
Jared Mercer <jaredthebackpacker@gmail.com>
2024-08-09 11:23:36 -0700

    chore: update revalidate endpoint to log

--- app/api/deals/revalidate/route.ts
+++ app/api/deals/revalidate/route.ts
@@ -3,5 +3,7 @@ import { revalidatePath } from "next/cache";
 export async function GET() {
   revalidatePath("/deals");
 
+  console.log("-> Revalidating deals");
+
   return new Response("ok", { status: 200 });
 }
