27d4c659c049afa52de2b6e861d5d999e82fe7a3
Jared Mercer <jaredthebackpacker@gmail.com>
2024-08-17 16:45:09 -0700

    chore: update csp headers to remove deprecated directives

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 // next.config.js
 const path = require("path");
 
-// TODO: Update the script-src-elem to allow the vercel speed insights script
 const cspHeader = `
     default-src *;
     script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    script-src-elem 'self' 'unsafe-eval' 'unsafe-inline' https://va.vercel-scripts.com/v1/script.debug.js https://va.vercel-scripts.com/v1/speed-insights/script.debug.js;
+    script-src-elem 'self' 'unsafe-inline' https://va.vercel-scripts.com/v1/script.debug.js https://va.vercel-scripts.com/v1/speed-insights/script.debug.js;
     style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
     img-src 'self' blob: data: https://lh3.googleusercontent.com;
     font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
@@ -13,7 +12,6 @@ const cspHeader = `
     base-uri 'self';
     form-action 'self';
     frame-ancestors 'self';
-    block-all-mixed-content;
     upgrade-insecure-requests;
 `;
 
