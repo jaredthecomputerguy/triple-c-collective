577d186a89df1308122ab93003c3325dc752703c
Jared Mercer <jaredthebackpacker@gmail.com>
2024-08-26 11:37:19 -0700

    chore: update lighthouse report runner

--- lighthouse/run-reports.mjs
+++ lighthouse/run-reports.mjs
@@ -1,5 +1,5 @@
-import { mkdir, writeFile } from "node:fs/promises";
-import { existsSync, rmdir } from "node:fs";
+import { mkdir, writeFile, rm } from "node:fs/promises";
+import { existsSync } from "node:fs";
 
 import * as chromeLauncher from "chrome-launcher";
 import lighthouse from "lighthouse";
@@ -21,14 +21,24 @@ const routes = [
 
 console.log("Running lighthouse for", routes);
 
+let index = 0;
+const total = routes.length;
+
 async function main() {
+  console.time("lighthouse");
   const outPath = "./lighthouse/reports";
 
   if (existsSync(outPath)) {
-    rmdir(outPath, { recursive: true, force: true });
+    try {
+      await rm(outPath, { recursive: true, force: true });
+    } catch (e) {
+      console.log("Error removing reports", e);
+    }
   }
 
   await mkdir(outPath);
+
+  generateIndex(outPath, routes);
 
   for (let idx = 0; idx < routes.length; idx++) {
     const route = routes[idx];
@@ -50,18 +60,67 @@ async function main() {
 
     const fileName =
       route === ""
-        ? `${outPath}/index.html`
+        ? `${outPath}/root.html`
         : `${outPath}/${route.replace(/\//gi, "_")}.html`;
-
-    await writeFile(fileName, reportHtml, {
-      flag: "w",
-      encoding: "utf-8",
-    });
-
-    console.log("Report is done for", runnerResult.lhr.finalDisplayedUrl);
+    try {
+      await writeFile(fileName, reportHtml, {
+        flag: "w",
+        encoding: "utf-8",
+      });
+      index++;
+      console.log(
+        "\nReport is done for",
+        runnerResult.lhr.finalDisplayedUrl,
+        `Progress: ${index}/${total}`,
+        "\n--------------------\n",
+      );
+    } catch (e) {
+      console.log("Error writing report", e);
+      chrome.kill();
+    }
 
     chrome.kill();
   }
+
+  console.log("\x1b[32;1;4mFinished running lighthouse.");
+  console.timeEnd("lighthouse");
+}
+
+function generateIndex(outPath, routes) {
+  const indexHtml = `<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Lighthouse Reports</title>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css"
+  >
+  <style>
+    .pt-8 {
+      padding-top: 2rem;
+    }
+  </style>
+</head>
+<body class="container pt-8">
+  <hgroup>
+    <h1>Lighthouse Reports</h1>
+    <p>This is a collection of lighthouse reports for the www.triplecolllective.com website.</p>
+  </hgroup>
+  <hr />
+  <h2>Reports</h2>
+  <ul>
+    ${routes.map((route) => `<li><a href="${route == "" ? "./root" : route.replace(/\//gi, "_")}.html">${route == "" ? "/" : route}</a></li>`).join("\n")}
+  </ul>
+</body>
+</html>`;
+
+  writeFile(`${outPath}/index.html`, indexHtml, {
+    flag: "w",
+    encoding: "utf-8",
+  });
 }
 
 main();
