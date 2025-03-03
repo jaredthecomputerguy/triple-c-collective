de85013d79ece866db92bc1e4f4f38fdef78985e
Jared Mercer <jaredthebackpacker@gmail.com>
2025-03-03 09:55:31 -0800

    chore: swap the stiiizy banner for the tt

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -93,7 +93,7 @@ export const Header = () => {
   return (
     <header className="sticky top-0 z-40 bg-[#fefefe] shadow">
       <TopBanner
-        active={true}
+        active={false}
         className="group bg-black py-4 transition hover:bg-white"
         closeBtnClass="text-white transition group-hover:text-black"
         onTouchStart={handleStiiizyBannerEvent}
@@ -105,7 +105,7 @@ export const Header = () => {
         <StiiizyBanner image={stiiizyLogo} />
       </TopBanner>
       <TrapTakeoverBanner
-        active={false}
+        active={true}
         bannerText="Trap Takeover - March 7th"
       />
       <div className="min-w-screen group sticky top-0 bg-primary-purple">
