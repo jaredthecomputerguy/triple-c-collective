8a5c70ee8046c675c40c682a90fdc1af89d6fc73
Jared Mercer <jaredthebackpacker@gmail.com>
2025-03-17 09:10:34 -0700

    feat: turn on st patricks day banner

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -60,12 +60,12 @@ export const Header = () => {
 
   return (
     <header className="sticky top-0 z-40 bg-[#fefefe] shadow">
-      <StiiizyBanner active={true} />
+      <StiiizyBanner active={false} />
       <TrapTakeoverBanner
         active={false}
         bannerText="Trap Takeover - March 21st"
       />
-      <StPatricksBanner active={false} />
+      <StPatricksBanner active={true} />
       <div className="min-w-screen group sticky top-0 bg-primary-purple">
         <div className="flex justify-between bg-primary-purple px-4 py-2 text-sm text-[#fefefe] md:hidden">
           <a
