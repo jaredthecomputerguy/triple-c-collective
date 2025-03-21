243bb343e321ec4bfdb91475ec9f0e12b436cc14
Jared Mercer <jaredthebackpacker@gmail.com>
2025-03-21 12:06:55 -0700

    chore: temp remove the newsletter form, update the text on the tt banner

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -60,10 +60,7 @@ export const Header = () => {
   return (
     <header className="sticky top-0 z-40 bg-[#fefefe] shadow">
       <StiiizyBanner active={false} />
-      <TrapTakeoverBanner
-        active={true}
-        bannerText="Trap Takeover - Today 12-6PM"
-      />
+      <TrapTakeoverBanner active={true} bannerText="Trap Takeover - LIVE NOW" />
       <div className="min-w-screen group sticky top-0 bg-primary-purple">
         <div className="flex justify-between bg-primary-purple px-4 py-2 text-sm text-[#fefefe] md:hidden">
           <a
