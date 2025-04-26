c3444ec99f7fc6b9cf4c18bc78c47e866567e897
Jared Mercer <jaredthebackpacker@gmail.com>
2025-04-26 11:28:22 -0700

    chore: add newsletter form to the footer of the layout

--- app/_components/newsletter-banner.tsx
+++ app/_components/newsletter-banner.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { TopBanner } from "./top-banner";
 
 interface NewsletterBannerProps {
@@ -5,27 +6,11 @@ interface NewsletterBannerProps {
 }
 
 export const NewsletterBanner = ({ active }: NewsletterBannerProps) => {
-  if (!active) {
-    return null;
-  }
-
-  function scrollNewsletterIntoView() {
-    const newsletter = document.querySelector("#newsletter");
-
-    if (!newsletter) {
-      throw new Error("Couldn't find any element with an id of 'newsletter'");
-    }
-
-    newsletter.scrollIntoView({
-      behavior: "smooth",
-    });
-  }
-
   return (
-    <TopBanner active={active} className="py-4 text-black">
-      <button className="text-black" onClick={scrollNewsletterIntoView}>
-        Newsletter
-      </button>
+    <TopBanner active={active} className="py-4">
+      <Link className="text-black" href="#newsletter">
+        Check out our newsletter!
+      </Link>
     </TopBanner>
   );
 };
