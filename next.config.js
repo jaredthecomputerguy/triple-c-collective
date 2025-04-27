496541b73b804c36f24f1a0b6eae8dd3d84cf369
Jared Mercer <jaredthebackpacker@gmail.com>
2025-04-27 15:26:58 -0700

    chore: update packages

--- next.config.js
+++ next.config.js
@@ -14,13 +14,6 @@ const cspHeader = `
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: [
-      "@react-email/components",
-      "@react-email/render",
-      "@react-email/tailwind",
-    ],
-  },
   images: {
     remotePatterns: [
       {
