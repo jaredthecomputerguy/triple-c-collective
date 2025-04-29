f9b93e94fa412a56db2d4e3ddf6866b02f9d34b2
Jared Mercer <jaredthebackpacker@gmail.com>
2025-04-28 18:49:42 -0700

    chore: remove /newsletter from sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -63,12 +63,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.2,
     },
     {
-      url: "https://tripleccollective.com/newsletter",
-      lastModified: new Date(),
-      changeFrequency: "yearly",
-      priority: 0.2,
-    },
-    {
       url: "https://tripleccollective.com/newsletter/unsubscribe",
       lastModified: new Date(),
       changeFrequency: "yearly",
