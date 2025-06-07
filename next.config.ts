4cc99e18dbb4c74bad3edcfdda556f3c5d8f91b2
Jared Mercer <jaredthebackpacker@gmail.com>
2025-06-07 12:33:40 -0700

    chore: change csp header to be more secure

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,19 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const cspHeader = `
-    default-src *;
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    script-src-elem 'self' 'unsafe-inline' https://va.vercel-scripts.com/v1/script.debug.js https://va.vercel-scripts.com/v1/speed-insights/script.debug.js;
-    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-    img-src 'self' blob: data: https://lh3.googleusercontent.com https://images.unsplash.com;
-    font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
-    object-src 'self';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'self';
-    upgrade-insecure-requests;
+  default-src 'self';
+  script-src 'self' ${isDev ? "'unsafe-eval'" : ""} 'unsafe-inline' https://va.vercel-scripts.com;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  img-src 'self' data: blob: https://lh3.googleusercontent.com https://images.unsplash.com;
+  font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
+  object-src 'none';
+  frame-src 'self' https://www.google.com https://calendar.google.com;
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'self';
+  upgrade-insecure-requests;
 `.replace(/\n/g, "");
 
 const nextConfig: NextConfig = {
