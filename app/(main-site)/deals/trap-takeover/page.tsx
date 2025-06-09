37dac938b29582cd8fa1b8704ab9f53a37688cf4
Jared Mercer <jaredthebackpacker@gmail.com>
2025-06-09 16:38:40 -0700

    feat: add tt brands for 0620, add a section to the tt page for a special promo (this time it's an art raffle)

--- app/(main-site)/deals/trap-takeover/page.tsx
+++ app/(main-site)/deals/trap-takeover/page.tsx
@@ -10,6 +10,8 @@ import underdogGrillLogo from "@/public/images/trap-takeover/underdog-grill.jpg"
 import { FacebookIcon } from "@/app/_components/icons/facebook-icon";
 import { InstagramIcon } from "@/app/_components/icons/instagram-icon";
 
+// TODO: Should probably refactor all of the sub-components to be their own files with their associated constants, this file is a mess
+
 // SHOW_PAGE determines whether to show the page or not
 const SHOW_PAGE = true;
 
@@ -34,10 +36,23 @@ const HAVE_GIFT_BAGS = false;
 // HAVE_FREE_FOOD determines whether we're doing the free food promo
 const HAVE_FREE_FOOD = false;
 
+// HAVE_SPECIAL_PROMO determines whether we have a special promo for the upcoming sale (i.e. non-cannabis raffle)
+const HAVE_SPECIAL_PROMO = true;
+
+const SPECIAL_PROMO_IMAGE_PATH = "/images/trap-takeover/reggie-painting.jpg";
+
 const TRAP_TAKEOVER_DATE_STRING = formatDate("06/20/2025");
 
 // Pass `undefined` as the last arg to show the 'more brands coming soon...'
-const featuredBrands = getFeaturedBrands(undefined);
+const featuredBrands = getFeaturedBrands(
+  "Akwaaba",
+  "Midsfactory",
+  "Dompen",
+  "Green River Extracts",
+  "Koa Cannabis Co.",
+  "Big Boy Dro",
+  "Park Jams",
+);
 
 export async function generateMetadata(): Promise<Metadata> {
   const trapTakeoverDate = new Date(TRAP_TAKEOVER_DATE_STRING);
@@ -220,6 +235,65 @@ export default function TrapTakeoverPage() {
           </>
         )}
 
+        {HAVE_SPECIAL_PROMO && (
+          <>
+            <h3 className="font-logo py-8 pb-12 text-center text-3xl font-semibold md:text-left">
+              Art Raffle Giveaway
+            </h3>
+            <section className="radial-gradient flex flex-col items-center gap-4 rounded-lg bg-yellow-600 p-8 text-white md:flex-row">
+              <div className="flex flex-col items-center">
+                <ImageViewer
+                  className="mx-auto my-8 w-full max-w-2xl cursor-pointer rounded-xl"
+                  src={SPECIAL_PROMO_IMAGE_PATH}
+                  alt="Painting by Reggie Baker"
+                  width={120}
+                  height={120}
+                />
+                <div className="flex gap-2">
+                  {/*
+                  TODO: Add a link to another of Reggie's socials when I see him!
+                  <Link
+                    href="https://www.facebook.com/UnderdogGrill?mibextid=wwXIfr"
+                    target="_blank"
+                    className="group rounded-full bg-white p-1 hover:bg-blue-600"
+                  >
+                    <FacebookIcon className="transition-all group-hover:fill-white" />
+                  </Link>
+                  */}
+                  <Link
+                    href="https://www.instagram.com/reggiebak3r/"
+                    target="_blank"
+                    className="group rounded-full bg-white p-1 hover:bg-gradient-to-br hover:from-[#f9ce34] hover:via-[#ee2a7b] hover:to-[#6228d7]"
+                  >
+                    <InstagramIcon className="transition-all group-hover:fill-white" />
+                  </Link>
+                </div>
+              </div>
+              <div>
+                <p className="mb-2 text-center text-xl font-semibold md:text-left">
+                  Win an original painting by{" "}
+                  <strong className="font-bold">Reggie Baker!</strong>
+                </p>
+                <div className="flex flex-col gap-2 text-left text-base">
+                  <p>
+                    Enter our special raffle during the Trap Takeover sale for a
+                    chance to win a one-of-a-kind painting created by talented
+                    local artist Reggie Baker.
+                  </p>
+                  <p>
+                    Every qualifying purchase earns you raffle entries, the more
+                    you buy, the better your odds!
+                  </p>
+                  <p>
+                    The lucky winner will be announced at the end of the event.
+                    Be sure to stick around or keep an eye on your phone!
+                  </p>
+                </div>
+              </div>
+            </section>
+          </>
+        )}
+
         <section className="py-8">
           <h3 className="font-logo py-4 text-center text-3xl font-semibold md:text-left">
             Featured Brands
@@ -293,6 +367,7 @@ export default function TrapTakeoverPage() {
             </video>
           </section>
         )}
+
         {HAVE_FLYER && (
           <section className="pb-8">
             <h3 className="font-logo py-4 text-center text-3xl font-semibold md:text-left">
