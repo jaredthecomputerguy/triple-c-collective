276bb8bc20dd2af375de5d3b7b89996f4bbb28ad
Jared Mercer <jaredthebackpacker@gmail.com>
2025-07-07 09:35:06 -0700

    chore(trap-takeover): remove trap takeover banner, stub out next tt page (#139)
    * chore(security): add some extra secruity headers
    
    * chore(trap-takeover): remove the trap takeover banner, stub out the page for the next one

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,30 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: cspHeader,
           },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=()", // adjust as needed
+          },
         ],
       },
     ];
