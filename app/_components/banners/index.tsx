dba5a84b98f07f038f00c59be8521860dc1447ca
Jared Mercer <jaredthebackpacker@gmail.com>
2025-07-12 17:47:12 -0700

    chore(refactor): change all of the imports in the banner dir to path aliases, like the rest of the project

--- app/_components/banners/index.tsx
+++ app/_components/banners/index.tsx
@@ -1,18 +1,18 @@
 import type { ComponentType } from "react";
 
-import { FourTwentyBanner } from "./4-20-banner";
-import { ChristmasBanner } from "./christmas-banner";
-import { CloneBanner } from "./clone-banner";
-import { MemorialDayBanner } from "./memorial-day-banner";
-import { MothersDayBanner } from "./mothers-day-banner";
-import { FathersDayBanner } from "./fathers-day-banner";
-import { NewYearBanner } from "./new-year-banner";
-import { NewsletterBanner } from "./newsletter-banner";
-import { SnapchatBanner } from "./snapchat-banner";
-import { StPatricksBanner } from "./st-patricks-banner";
-import { StiiizyBanner } from "./stiiizy-banner";
-import { TrapTakeoverBanner } from "./trap-takeover-banner";
-import { SevenTenSaleBanner } from "./710-sale-banner/index";
+import { TrapTakeoverBanner } from "@/app/_components/banners/trap-takeover-banner";
+import { StiiizyBanner } from "@/app/_components/banners/stiiizy-banner";
+import { CloneBanner } from "@/app/_components/banners/clone-banner";
+import { FourTwentyBanner } from "@/app/_components/banners/4-20-banner";
+import { ChristmasBanner } from "@/app/_components/banners/christmas-banner";
+import { MemorialDayBanner } from "@/app/_components/banners/memorial-day-banner";
+import { MothersDayBanner } from "@/app/_components/banners/mothers-day-banner";
+import { FathersDayBanner } from "@/app/_components/banners/fathers-day-banner";
+import { NewYearBanner } from "@/app/_components/banners/new-year-banner";
+import { NewsletterBanner } from "@/app/_components/banners/newsletter-banner";
+import { SevenTenSaleBanner } from "@/app/_components/banners/710-sale-banner/";
+import { SnapchatBanner } from "@/app/_components/banners/snapchat-banner";
+import { StPatricksBanner } from "@/app/_components/banners/st-patricks-banner";
 
 type PropsOf<T> = T extends ComponentType<infer P> ? P : never;
 
