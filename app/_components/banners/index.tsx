e14aa5334d727a2071bf848afd4f69053dc70f78
Jared Mercer <jaredthebackpacker@gmail.com>
2025-10-25 10:20:15 -0700

    chore(sale): put weekend stiiizy banner up (#202)

--- app/_components/banners/index.tsx
+++ app/_components/banners/index.tsx
@@ -66,7 +66,7 @@ const bannerConfig: [
       bannerSubText: "TODAY | 12-6PM"
     }
   },
-  { Component: StiiizyBanner, active: false, order: 1, props: {} },
+  { Component: StiiizyBanner, active: true, order: 1, props: {} },
   { Component: CloneBanner, active: false, order: 50, props: {} },
   { Component: FourTwentyBanner, active: false, order: 50, props: {} },
   { Component: ChristmasBanner, active: false, order: 50, props: {} },
