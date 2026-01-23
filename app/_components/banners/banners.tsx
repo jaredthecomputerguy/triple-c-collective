7dac98a2a976282cdc909e64af373307d44e4159
Jared Mercer <jaredthebackpacker@gmail.com>
2026-01-23 10:19:13 -0800

    chore(sale): 012326 trap takeover is here (#260)

--- app/_components/banners/banners.tsx
+++ app/_components/banners/banners.tsx
@@ -84,7 +84,7 @@ const bannerConfig = [
     order: Order.Second,
     props: {
       bannerText: "Trap Takeover Sale",
-      bannerSubText: "Fri, January 23rd | 12-6PM",
+      bannerSubText: "TODAY | 12-6PM",
       mini: true
     }
   }),
