a67728e0e0c69d62f2fd6502c2daa684026c61db
Jared Mercer <jaredthebackpacker@gmail.com>
2026-02-02 10:08:17 -0800

    chore(sale): stiiizy weekend sale is over, swap to 020626 trap takeover (#267)

--- app/_components/banners/banners.tsx
+++ app/_components/banners/banners.tsx
@@ -80,17 +80,17 @@ const bannerConfig = [
   }),
   generateBanner({
     Component: TrapTakeoverBanner,
-    active: false,
+    active: true,
     order: Order.Second,
     props: {
       bannerText: "Trap Takeover Sale",
-      bannerSubText: "TODAY | 12-6PM",
+      bannerSubText: "Fri Feb. 6th | 12-6PM",
       mini: true
     }
   }),
   generateBanner({
     Component: StiiizyBanner,
-    active: true,
+    active: false,
     order: Order.Second
   }),
   generateBanner({ Component: CloneBanner }),
