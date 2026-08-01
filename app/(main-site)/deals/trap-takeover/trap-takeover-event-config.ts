eae141748010b0f0a5115aa5b20c02e3b950f93b
Jared Mercer <jaredthebackpacker@gmail.com>
2026-08-01 12:09:57 -0700

    chore(sale): 080726 trap takeover (#359)

--- app/(main-site)/deals/trap-takeover/trap-takeover-event-config.ts
+++ app/(main-site)/deals/trap-takeover/trap-takeover-event-config.ts
@@ -9,18 +9,25 @@ const eventData: TrapTakeoverInput = {
   year: 2026,
   month: 8,
   day: 7,
-  featuredBrands: ["Brands coming soon..."],
+  featuredBrands: [
+    "Dompen",
+    "Geek THCX",
+    "Koa Cannabis Co.",
+    "Together Canna Supply",
+    "Hashtag",
+    "High 90's",
+  ],
   flags: {
     /* TRUE FLAGS */
     featuredBrands: true,
-    flyer: false,
+    flyer: true,
     /* FALSE FLAGS */
     giftBags: false,
     freeFood: false,
     specialArtPromo: false,
     specialPromo: false,
     video: false,
-    individualDeals: true,
+    individualDeals: false,
   },
   deals,
   numberOfGiftBags: 0,
