57dc83eb117f39fab968723885a13dd10b37d5b2
Jared Mercer <jaredthebackpacker@gmail.com>
2026-08-22 10:06:00 -0700

    chore(sale): stiiizy-weekend-end (#369)

--- app/(main-site)/deals/trap-takeover/trap-takeover-event-config.ts
+++ app/(main-site)/deals/trap-takeover/trap-takeover-event-config.ts
@@ -8,18 +8,11 @@ const deals: IndividualDeal[] = [];
 const eventData: TrapTakeoverInput = {
   year: 2026,
   month: 8,
-  day: 21,
-  featuredBrands: [
-    "High 90's",
-    "Hypnotic",
-    "Park Jams",
-    "Box Lunch",
-    "Big Boy Dro",
-    "Together Canna Supply",
-  ],
+  day: 28,
+  featuredBrands: ["Brands coming soon..."],
   flags: {
     featuredBrands: true,
-    flyer: true,
+    flyer: false,
     /* Other flags */
     giftBags: false,
     freeFood: false,
